c1bc78e020db5a1ab73d7341d86fca3c671056d4
emmtqg <elizabeth.m.mcmahon@gmail.com>
2019-10-06 10:09:21 -0400

    Prior to route module - renamed files

---
+++ src/config/Users.ts
@@ -0,0 +1,13 @@
+{
+  "apiUrl": "https://jsonplaceholder.typicode.com/users",
+  "headers": [ 
+    { "field": "name", "display": "Name" }, 
+    { "field": "email", "display": "Email" }, 
+    { "field": "address.city", "display": "City" }, 
+    { "field": "company.name", "display": "Company"}
+  ],
+  "alignments": [ "left", "center", "right", "right" ],
+  "cols": [ 3, 3, 2, 4 ],
+  "title": "Users",
+  "searchCaseSensitive": false
+}
